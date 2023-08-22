ec5c3b277ac829078d09c7246c9a186b0525c0d9
Adekunle Adesoji <donsoj1st@byui.edu>
2023-08-22 12:16:03 +0100

    update form

--- js/main.js
+++ js/main.js
@@ -68,4 +68,18 @@ async function loadCountry() {
   console.log(Nationality);
 }
 
+form.addEventListener(
+  "submit",
+  (event) => {
+    const data = new FormData(form);
+    let output = "";
+    for (const entry of data) {
+      output = `${output}${entry[0]}=${entry[1]}\r`;
+    }
+    log.innerText = output;
+    event.preventDefault();
+  },
+  false
+);
+
 loadCountry();
