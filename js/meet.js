b2c5959634a28105e49c3026706735c66cd7d1a6
Adekunle Adesoji <donsoj1st@byui.edu>
2024-03-07 17:13:02 +0100

    update

---
+++ js/meet.js
@@ -0,0 +1,18 @@
+function ade() {
+  return 0;
+}
+const button = document.getElementById("submitBtn");
+
+button.addEventListener("click", async (value) => {
+  value.preventDefault();
+
+  // Email.send({
+  //   Host: "smtp.mail.yahoo.com",
+  //   Username: "btmhris@yahoo.com",
+  //   Password: "yhsxefbqrjlipvqj",
+  //   To: "donsoj1st@gmail.com",
+  //   From: "btmhris@yahoo.com",
+  //   Subject: "This is the subject",
+  //   Body: "And this is the body",
+  // }).then((message) => alert(message));
+});
